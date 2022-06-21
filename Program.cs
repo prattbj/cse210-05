7b59f2b62df285a0eb4e4016b4fb5217195978e7
JoshJ-WDD <joh21036@byui.edu>
2022-06-21 11:01:54 -0600

    Modified Program, deleted food

--- Program.cs
+++ Program.cs
@@ -19,9 +19,8 @@ namespace Unit05
         {
             // create the cast
             Cast cast = new Cast();
-            cast.AddActor("food", new Food());
-            cast.AddActor("snake", new Snake());
-            cast.AddActor("score", new Score());
+            cast.AddActor("player1", new Player());
+            cast.AddActor("player2", new Player());
 
             // create the services
             KeyboardService keyboardService = new KeyboardService();
