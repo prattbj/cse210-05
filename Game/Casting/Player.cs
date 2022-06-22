b2b2a51994d5136bf83f981edb0b6bd49900e243
JoshJ-WDD <joh21036@byui.edu>
2022-06-22 10:43:36 -0600

    Cycles should no longer spawn with a trail

--- Game/Casting/Player.cs
+++ Game/Casting/Player.cs
@@ -91,21 +91,22 @@ namespace Unit05.Game.Casting
         {
             int x = Constants.MAX_X / 2;
             int y = Constants.MAX_Y / 2;
+            
 
-            for (int i = 0; i < Constants.SNAKE_LENGTH; i++)
-            {
-                Point position = new Point(x - i * Constants.CELL_SIZE, y);
-                Point velocity = new Point(1 * Constants.CELL_SIZE, 0);
-                string text = i == 0 ? "8" : "#";
-                Color color = i == 0 ? Constants.YELLOW : Constants.GREEN;
+            // for (int i = 0; i < Constants.SNAKE_LENGTH; i++)
+            // {
+            //     Point position = new Point(x - i * Constants.CELL_SIZE, y);
+            //     Point velocity = new Point(1 * Constants.CELL_SIZE, 0);
+            //     string text = i == 0 ? "8" : "#";
+            //     Color color = i == 0 ? Constants.YELLOW : Constants.GREEN;
 
-                Actor segment = new Actor();
-                segment.SetPosition(position);
-                segment.SetVelocity(velocity);
-                segment.SetText(text);
-                segment.SetColor(color);
-                segments.Add(segment);
-            }
+            //     Actor segment = new Actor();
+            //     segment.SetPosition(position);
+            //     segment.SetVelocity(velocity);
+            //     segment.SetText(text);
+            //     segment.SetColor(color);
+            //     segments.Add(segment);
+            // }
         }
     }
 }
